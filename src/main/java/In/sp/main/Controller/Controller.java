7fd72c41128b50ba42f090c5289be02b613b627a
Er-rinkusingh <161230111+Er-rinkusingh@users.noreply.github.com>
2025-02-18 15:07:28 +0530

    .

--- src/main/java/In/sp/main/Controller/Controller.java
+++ src/main/java/In/sp/main/Controller/Controller.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/Api_data")
+@RequestMapping("/api")
 public class Controller {
 	
 	 @GetMapping("/showData")
