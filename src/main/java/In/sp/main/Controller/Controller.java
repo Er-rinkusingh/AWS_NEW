e903956eb848b49ee7098faa26ceb59f52e8e653
Er-rinkusingh <161230111+Er-rinkusingh@users.noreply.github.com>
2025-02-18 14:20:43 +0530

    First commit

--- src/main/java/In/sp/main/Controller/Controller.java
+++ src/main/java/In/sp/main/Controller/Controller.java
@@ -1,18 +1,18 @@
-package In.sp.main.Controller;
+package In.sp.main.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api_data")
+@RequestMapping("/Api_data")
 public class Controller {
 	
 	 @GetMapping("/showData")
 	    public String showData() {
 	        String data = "Hello, this is your console output!";
-	        System.out.println(data);
-	        System.out.println("First try for AWS");// Print data to the console
+	        System.out.println(data); // Print data to the console
+	        System.out.println("First try for aws...");
 	        return "Check the console for the printed data.";
 	    }
 
